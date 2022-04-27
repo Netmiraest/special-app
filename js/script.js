88fb0f551540dd5edff159f1d90e91a524ea2e81
Alexandr Kozlov <netmiraest@gmail.com>
2022-04-27 15:04:19 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// new Swiper('.image-slider', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   fadeEffect: {
+//     crossFade: true
+//   },
+//   virtualTranslate: true,
+//   autoplay: {
+//     delay: 3000,
+//     disableOnInteraction: false
+//   },
+//   speed: 800,
+//   effect: "fade"
+// });
+
+// new Swiper('.contact-slider__slider', {
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+//   slidesPerView: 10,
+//   slidesPerGroup: 10,
+//   spaceBetween: 18,
+//   grabCursor: true,
+//   breakpoints: {
+//     // when window width is >= 320px
+//     0: {
+//       slidesPerView: 2,
+//       slidesPerGroup: 2,
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       slidesPerView: 3,
+//       slidesPerGroup: 3,
+//     },
+//     // when window width is >= 640px
+//     600: {
+//       slidesPerView: 4,
+//       slidesPerGroup: 4,
+//     },
+//     900: {
+//       slidesPerView: 6,
+//       slidesPerGroup: 6,
+//     },
+//     1300: {
+//       slidesPerView: 8,
+//       slidesPerGroup: 8,
+//       spaceBetween: 18
+//     }
+//   }
+// });
+
+
+// new Swiper('.article-slider__swiper', {
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   grabCursor: true,
+//   spaceBetween: 30,
+// });
+// if (document.title == 'Home'|| document.title == 'Gallery') {
+// var mixer = mixitup('.gallery__inner', {
+//   load: {
+//     filter: '.living'
+//   }
+// });
+// }
+
+
